88de8af8075b164126f98af7823d30fb94511da5
StephenWilkens <stephenwilkens@gmail.com>
2023-02-22 16:38:44 -0700

    Add package_networks join table

---
+++ models/package_networks.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class package_networks extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  package_networks.init({
+    packageId: DataTypes.INTEGER,
+    networkId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'package_networks',
+  });
+  return package_networks;
+};
