3ef4585ef2a6aa2bc8465c2c36ccee176e3b540c
StephenWilkens <stephenwilkens@gmail.com>
2023-02-22 18:12:03 -0700

    Get test script to use test env

--- src/tests/controller.tests/networks.test.js
+++ src/tests/controller.tests/networks.test.js
@@ -1,2 +1,13 @@
-const request = require('supertest')
-const app = require("../../app")
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Networks controller", () => {
+  describe("GET Networks", () => {
+    describe("happy path", () => {
+      test("returns a status code 200", async () => {
+        const response = await request(app).get('/api/v1/networks');
+        expect(response.statusCode).toBe(200)
+      });
+    });
+  });
+});
