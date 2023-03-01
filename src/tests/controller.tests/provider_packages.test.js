5e3324c93323b5e406b69e0f8bf032cf52729a72
StephenWilkens <stephenwilkens@gmail.com>
2023-03-01 14:11:59 -0700

    Add getPackage controller test

--- src/tests/controller.tests/provider_packages.test.js
+++ src/tests/controller.tests/provider_packages.test.js
@@ -13,7 +13,21 @@ describe("Provider_packages controller", () => {
         expect(res.body.packages[0]).toHaveProperty("price");
         expect(res.body.packages[0]).toHaveProperty("createdAt");
         expect(res.body.packages[0]).toHaveProperty("updatedAt");
-      })
-    })
-  })
-})
+      });
+    });
+  });
+  describe("getPackage", () => {
+    describe("happy path", () => {
+      it("should return the correct package", async () => {
+        const res = await request(app).get("/api/v1/packages/1");
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty("package");
+        expect(res.body.package).toHaveProperty("id");
+        expect(res.body.package).toHaveProperty("title");
+        expect(res.body.package).toHaveProperty("price");
+        expect(res.body.package).toHaveProperty("createAt");
+        expect(res.body.package).toHaveProperty("updatedAt");
+      });
+    });
+  });
+});
